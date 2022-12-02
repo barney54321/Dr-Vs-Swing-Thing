8bd04afac7b8f2de05fbb3fc5d3b0f8d49cf466c
Andrew Esteban <andrewesteban1999@gmail.com>
2022-12-02 11:20:09 +1100

    Everything

---
+++ assets/js/dorang.js
@@ -0,0 +1,85 @@
+/*!
+=========================================================
+* Dorang Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+ // toggle 
+$(document).ready(function(){
+    
+    $('.search-toggle').click(function(){
+        $('.search-wrapper').toggleClass('show');
+    });
+
+    $('.modal-toggle').click(function(){
+        $('.modalBox').toggleClass('show');
+    })
+
+    $('.modalBox').click(function(){
+        $(this).removeClass('show');
+    });
+
+    $('.spinner').click(function(){
+        $(".theme-selector").toggleClass('show');
+    });
+    $('.light').click(function(){
+        $('body').addClass('light-theme');
+        $('body').removeClass('dark-theme');
+    });
+    $('.dark').click(function(){
+        $('body').toggleClass('dark-theme');
+        $('body').removeClass('light-theme');
+    });
+});
+
+
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+}); 
+
+
+
+function toggleGallery() {
+    let images = document.getElementsByClassName("extra-gallery");
+
+    let more = false;
+
+    for (let element of images) {
+        if (element.style.display == "block") {
+            element.style.display = "none";
+        } else {
+            element.style.display = "block";
+            more = true;
+        }
+    }
+
+    if (more) {
+        document.getElementById("gallery-toggle").innerText = "Show Less";
+    } else {
+        document.getElementById("gallery-toggle").innerText = "Show More";
+    }
+}
